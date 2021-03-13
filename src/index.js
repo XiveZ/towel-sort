ed9d6a8b8423f57930024b683d40951b855622c5
Nj <jumrsup@hi2.in>
2021-03-13 13:50:59 +0300

    feat: add code calculation result

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (typeof matrix == 'undefined') return []
+  let count = []; 
+  matrix.reduce((accumm, curent, index)=> {
+    if (index%2!=0){
+      count.push(...matrix[index].reverse())
+    }else{
+    count.push(...matrix[index])
+    }
+  },0)
+  return count
 }
